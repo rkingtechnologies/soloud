d7ef7cd475ebd0613a8bd85fc1dbdfff7a41da42
Ryan <ryankingnet@outlook.com>
2026-01-19 22:23:44 -0500

    removed bloat from tests dir

--- tests/enumerate/main.cpp
+++ tests/enumerate/main.cpp
@@ -22,98 +22,107 @@ freely, subject to the following restrictions:
    distribution.
 */
 
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "soloud.h"
 #include "soloud_thread.h"
 
-const char * getBackendEnumString(int aBackend)
-{
-	SOLOUD_ASSERT(SoLoud::Soloud::BACKEND_MAX == 17); // if this fails, this function needs adjustment
-	switch (aBackend)
-	{
-	case SoLoud::Soloud::AUTO: return "AUTO";
-	case SoLoud::Soloud::SDL1: return "SDL1";
-	case SoLoud::Soloud::SDL2: return "SDL2";
-	case SoLoud::Soloud::PORTAUDIO: return "PORTAUDIO";
-	case SoLoud::Soloud::WINMM: return "WINMM";
-	case SoLoud::Soloud::XAUDIO2: return "XAUDIO2";
-	case SoLoud::Soloud::WASAPI: return "WASAPI";
-	case SoLoud::Soloud::ALSA: return "ALSA";
-	case SoLoud::Soloud::JACK: return "JACK";
-	case SoLoud::Soloud::OSS: return "OSS";
-	case SoLoud::Soloud::OPENAL: return "OPENAL";
-	case SoLoud::Soloud::COREAUDIO: return "COREAUDIO";
-	case SoLoud::Soloud::OPENSLES: return "OPENSLES";
-	case SoLoud::Soloud::VITA_HOMEBREW: return "VITA_HOMEBREW";
-	case SoLoud::Soloud::NULLDRIVER: return "NULLDRIVER";
-	case SoLoud::Soloud::NOSOUND: return "NOSOUND";
-	case SoLoud::Soloud::MINIAUDIO: return "MINIAUDIO";
-	}
-	return "?!";
+
+const char *getBackendEnumString(int aBackend) {
+  SOLOUD_ASSERT(SoLoud::Soloud::BACKEND_MAX ==
+                17);  // if this fails, this function needs adjustment
+  switch (aBackend) {
+    case SoLoud::Soloud::AUTO:
+      return "AUTO";
+    case SoLoud::Soloud::SDL1:
+      return "SDL1";
+    case SoLoud::Soloud::SDL2:
+      return "SDL2";
+    case SoLoud::Soloud::PORTAUDIO:
+      return "PORTAUDIO";
+    case SoLoud::Soloud::WINMM:
+      return "WINMM";
+    case SoLoud::Soloud::XAUDIO2:
+      return "XAUDIO2";
+    case SoLoud::Soloud::WASAPI:
+      return "WASAPI";
+    case SoLoud::Soloud::ALSA:
+      return "ALSA";
+    case SoLoud::Soloud::JACK:
+      return "JACK";
+    case SoLoud::Soloud::OSS:
+      return "OSS";
+    case SoLoud::Soloud::OPENAL:
+      return "OPENAL";
+    case SoLoud::Soloud::COREAUDIO:
+      return "COREAUDIO";
+    case SoLoud::Soloud::OPENSLES:
+      return "OPENSLES";
+    case SoLoud::Soloud::VITA_HOMEBREW:
+      return "VITA_HOMEBREW";
+    case SoLoud::Soloud::NULLDRIVER:
+      return "NULLDRIVER";
+    case SoLoud::Soloud::NOSOUND:
+      return "NOSOUND";
+    case SoLoud::Soloud::MINIAUDIO:
+      return "MINIAUDIO";
+  }
+  return "?!";
 }
 
-const char * getChannelString(int aChannels)
-{
-	switch (aChannels)
-	{
-	case 8:
-		return " (7.1 surround)";
-	case 6:
-		return " (5.1 surround)";
-	case 4:
-		return " (quad)";
-	case 2:
-		return " (stereo)";
-	case 1:
-		return " (mono)";
-	}
-	return " (?!)";
+const char *getChannelString(int aChannels) {
+  switch (aChannels) {
+    case 8:
+      return " (7.1 surround)";
+    case 6:
+      return " (5.1 surround)";
+    case 4:
+      return " (quad)";
+    case 2:
+      return " (stereo)";
+    case 1:
+      return " (mono)";
+  }
+  return " (?!)";
 }
 
-int main(int argc, char *argv[])
-{
-	SoLoud::Soloud soloud;
+int main() {
+  SoLoud::Soloud soloud;
 
-	int i;
-	for (i = 0; i < SoLoud::Soloud::BACKEND_MAX; i++)
-	{
-		printf("-----\n"
-			"Backend %d:%s\n",
-			i, getBackendEnumString(i));
-		int res = soloud.init(0, i);
-		if (res == SoLoud::SO_NO_ERROR)
-		{
-			printf(
-				"ID:       %d\n"
-				"String:   \"%s\"\n"
-				"Rate:     %d\n"
-				"Buffer:   %d\n"
-				"Channels: %d%s (default)\n",
-				soloud.getBackendId(),
-				soloud.getBackendString() == 0 ? "(null)" : soloud.getBackendString(),
-				soloud.getBackendSamplerate(),
-				soloud.getBackendBufferSize(),
-				soloud.getBackendChannels(),
-				getChannelString(soloud.getBackendChannels()));
-			soloud.deinit();
-			int j;
-			for (j = 1; j < 12; j++)
-			{
-				int res = soloud.init(0, i, 0, 0, j);
-				if (res == SoLoud::SO_NO_ERROR && soloud.getBackendChannels() == j)
-				{
-					printf("Channels: %d%s\n", soloud.getBackendChannels(), getChannelString(soloud.getBackendChannels()));
-					soloud.deinit();					
-					SoLoud::Thread::sleep(200);
-				}
-			}
-		}
-		else
-		{
-			printf("Failed: %s\n", soloud.getErrorString(res));
-		}
-	}
-	return 0;
+  int i;
+  for (i = 0; i < SoLoud::Soloud::BACKEND_MAX; i++) {
+    printf(
+      "-----\n"
+      "Backend %d:%s\n",
+      i, getBackendEnumString(i));
+    int res = soloud.init(0, i);
+    if (res == SoLoud::SO_NO_ERROR) {
+      printf(
+        "ID:       %d\n"
+        "String:   \"%s\"\n"
+        "Rate:     %d\n"
+        "Buffer:   %d\n"
+        "Channels: %d%s (default)\n",
+        soloud.getBackendId(),
+        soloud.getBackendString() == 0 ? "(null)" : soloud.getBackendString(),
+        soloud.getBackendSamplerate(), soloud.getBackendBufferSize(),
+        soloud.getBackendChannels(),
+        getChannelString(soloud.getBackendChannels()));
+      soloud.deinit();
+      int j;
+      for (j = 1; j < 12; j++) {
+        int res = soloud.init(0, i, 0, 0, j);
+        if (res == SoLoud::SO_NO_ERROR && soloud.getBackendChannels() == j) {
+          printf("Channels: %d%s\n", soloud.getBackendChannels(),
+            getChannelString(soloud.getBackendChannels()));
+          soloud.deinit();
+          SoLoud::Thread::sleep(200);
+        }
+      }
+    } else {
+      printf("Failed: %s\n", soloud.getErrorString(res));
+    }
+  }
+  return 0;
 }
